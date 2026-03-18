e6f7ee376a7304f6c63e8d493a4e46611d066d57
NightmareAway <1944704123@qq.com>
2026-03-18 20:49:24 +0800

    一.项目初始化（后端） 1.初始化配置，配置文件 2.引入 aop切面 hutool工具库 knife4j接口文档 3.引入基础通用文件，如自定义错误类，自定义异常等

---
+++ src/main/java/cn/ko_ai_code/com/koaicode/KoAiCodeApplication.java
@@ -0,0 +1,13 @@
+package cn.ko_ai_code.com.koaicode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoAiCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoAiCodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/ko_ai_code/com/koaicode/KoAiCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.ko_ai_code.com.koaicode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoAiCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
