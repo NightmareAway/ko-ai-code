76f3cc252c97b333267b2dd54571345bb3be01ca
NightmareAway <1944704123@qq.com>
2026-03-19 22:30:25 +0800

    三.构建项目后端用户模块基础化 1.用户登录注册，返回id，用户脱敏，使用md5加盐加密 2.使用用户常量键，将用户信息保存在session里 3.添加aop切面，在添加注解的地方校验权限。目前只有两种角色：user和admin。 4.使用jackson将返回的Long类型转化为字符串，防止精度丢失 5.管理员基础操作接口

---
+++ src/main/java/cn/ko_ai_code/com/koaicode/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package cn.ko_ai_code.com.koaicode.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import cn.ko_ai_code.com.koaicode.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author ko
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ src/main/java/cn/ko_ai_code/com/koaicode/service/UserService.java
@@ -0,0 +1,85 @@
+package cn.ko_ai_code.com.koaicode.service;
+
+import cn.ko_ai_code.com.koaicode.model.dto.user.UserQueryRequest;
+import cn.ko_ai_code.com.koaicode.model.vo.LoginUserVO;
+import cn.ko_ai_code.com.koaicode.model.vo.UserVO;
+import com.mybatisflex.core.query.QueryWrapper;
+import com.mybatisflex.core.service.IService;
+import cn.ko_ai_code.com.koaicode.entity.User;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.util.List;
+
+/**
+ * 用户 服务层。
+ *
+ * @author ko
+ */
+public interface UserService extends IService<User> {
+    long userRegister(String userAccount, String userPassword, String checkPassword);
+
+    /**
+     * 获取脱敏的已登录用户信息
+     *
+     * @return 脱敏的用户信息
+     */
+    LoginUserVO getLoginUserVO(User user);
+
+    /**
+     * 获取加密密码
+     *
+     * @param userPassword 用户密码
+     * @return 加密后密码
+     */
+    String getEncryptPassword(String userPassword);
+
+    /**
+     * 用户登录
+     *
+     * @param userAccount  用户账户
+     * @param userPassword 用户密码
+     * @param request
+     * @return 脱敏后的用户信息
+     */
+    LoginUserVO userLogin(String userAccount, String userPassword, HttpServletRequest request);
+
+    /**
+     * 获取当前登录用户
+     *
+     * @param request
+     * @return
+     */
+    User getLoginUser(HttpServletRequest request);
+
+    /**
+     * 用户注销
+     *
+     * @param request
+     * @return
+     */
+    boolean userLogout(HttpServletRequest request);
+
+    /**
+     * 查看用户
+     *
+     * @param user
+     * @return
+     */
+    UserVO getUserVO(User user);
+
+    /**
+     * 获取用户列表
+     *
+     * @param userList
+     * @return
+     */
+    List<UserVO> getUserVOList(List<User> userList);
+
+    /**
+     * 将请求转为QueryWapper
+     *
+     * @param userQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(UserQueryRequest userQueryRequest);
+}
