6d0eb62960c85ac767bc0e38f4f8d8f7be107d1c
NightmareAway <1944704123@qq.com>
2026-03-19 22:30:21 +0800

    三.构建项目后端用户模块基础化 1.用户登录注册，返回id，用户脱敏，使用md5加盐加密 2.使用用户常量键，将用户信息保存在session里 3.添加aop切面，在添加注解的地方校验权限。目前只有两种角色：user和admin。 4.使用jackson将返回的Long类型转化为字符串，防止精度丢失 5.管理员基础操作接口

--- src/main/java/cn/ko_ai_code/com/koaicode/KoAiCodeApplication.java
+++ src/main/java/cn/ko_ai_code/com/koaicode/KoAiCodeApplication.java
@@ -1,9 +1,11 @@
 package cn.ko_ai_code.com.koaicode;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("cn.ko_ai_code.com.koaicode.mapper")
 public class KoAiCodeApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/cn/ko_ai_code/com/koaicode/model/dto/user/UserLoginRequest.java
@@ -0,0 +1,21 @@
+package cn.ko_ai_code.com.koaicode.model.dto.user;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class UserLoginRequest implements Serializable {
+
+    private static final long serialVersionUID = 3191241716373120793L;
+
+    /**
+     * 账号
+     */
+    private String userAccount;
+
+    /**
+     * 密码
+     */
+    private String userPassword;
+}
