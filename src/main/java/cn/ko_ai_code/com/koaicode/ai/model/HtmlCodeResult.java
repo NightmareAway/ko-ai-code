b5d56b6bc59aef1da40452eada677a9b3a3e243b
NightmareAway <1944704123@qq.com>
2026-03-23 20:14:10 +0800

    四、添加langchain4j并实现流式输出 1.目前支持两种生成格式：HTML和多文件格式 2.创建对应门面类，方便调用 3.为deepseek添加结构化返回结果（流式响应不支持） 4.为流式响应使用Reactor，会在不断返回时拼接代码，最后形成持久化文件存入后端

---
+++ src/main/java/cn/ko_ai_code/com/koaicode/ai/model/HtmlCodeResult.java
@@ -0,0 +1,11 @@
+package cn.ko_ai_code.com.koaicode.ai.model;
+
+import lombok.Data;
+
+@Data
+public class HtmlCodeResult {
+
+    private String htmlCode;
+
+    private String description;
+}
---
+++ src/main/java/cn/ko_ai_code/com/koaicode/ai/model/MultiFileCodeResult.java
@@ -0,0 +1,15 @@
+package cn.ko_ai_code.com.koaicode.ai.model;
+
+import lombok.Data;
+
+@Data
+public class MultiFileCodeResult {
+
+    private String htmlCode;
+
+    private String cssCode;
+
+    private String jsCode;
+
+    private String description;
+}
